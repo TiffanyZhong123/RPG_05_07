271260e99cd0aabde77bd221f137c77f8f229d2c
Sophie Watwood <swatwood@stuy.edu>
2017-12-13 17:03:03 -0500

    Protagonist abstractified, Warrior made usable as a concrete subclass. No other files modified.

--- RPGv3/Protagonist.java
+++ RPGv3/Protagonist.java
@@ -1,6 +1,31 @@
-public class Protagonist extends Character {
+public abstract class Protagonist extends Character {
+ 
+/***************************
+ * error from compiling Protagoinst w abstract methods without declaring it abstract:
+ *Protagonist.java:1: error: Protagonist is not abstract and does not override abstract method normalize() in Protagonist
+ *public  class Protagonist extends Character {
+ *        ^
+ *1 error
+
+ * error from compiling warrior with no override:
+ *Warrior.java:1: error: Warrior is not abstract and does not override abstract method normalize() in Protagonist
+ *public class Warrior extends Protagonist {
+ *       ^
+ *1 error
+ *[1]+  Done                    gedit Warrior.java
+
+ *trying to instantiate Protagonist:
+ *./Protagonist.java:46: error: Protagonist is abstract; cannot be instantiated
+ *    Protagonist James = new Protagonist();
+ *                        ^
+ *1 error
+
+
+ **/
     public String name;
 
+    public abstract String toString();
+/**
     public String toString(){
         return name + ", who has strength " 
              + strength + ", HP "
@@ -8,6 +33,7 @@ public class Protagonist extends Character {
              + defense + ", and an attackRating of " 
              + attackRating;
     }
+*/
 
     public Protagonist( String n, int a, int b, int c, double d) {
 	name = n;
@@ -21,6 +47,12 @@ public class Protagonist extends Character {
 	return name;
     }
 
+    public abstract void specialize();
+
+    public abstract void normalize();
+  
+
+/**
     public void specialize() {
         defense -= 5;
 	attackRating += 0.5;
@@ -29,5 +61,7 @@ public class Protagonist extends Character {
     public void normalize() {
 	defense = 40;
 	attackRating = 0.4;
-    }
+}
+*/   
+ 
 }
--- RPGv3/Warrior.java
+++ RPGv3/Warrior.java
@@ -3,7 +3,17 @@ public class Warrior extends Protagonist {
 	super( n, 100, 150, 60, 0.4);
     }
 
+    public void specialize() {
+        defense -= 5;
+	attackRating += 0.5;
+    }
+
+    public void normalize() {
+	defense = 40;
+	attackRating = 0.4;
+    }
+
     public String toString() {
-        return "The Warrior: " + super.toString();
+        return "The Warrior: " + name;
     }
 }
